6ddf2d2abf2cbe3a9969d7b9d294fc72c7e89e81
habin <dhdudgns6@gmail.com>
2023-12-08 16:51:39 +0900

    join-1

--- awesomeGroup/src/main/java/com/project/awesomegroup/controller/user/UserController.java
+++ awesomeGroup/src/main/java/com/project/awesomegroup/controller/user/UserController.java
@@ -6,10 +6,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +24,11 @@ public class UserController {
     public List<User> userAllSelect(){
         return userService.findAll();
         //return new ArrayList<>();
+    }
+
+    @PostMapping
+    public User userJoin(@RequestBody User user){
+        return userService.join(user);
     }
 
 }
--- awesomeGroup/src/main/java/com/project/awesomegroup/service/UserService.java
+++ awesomeGroup/src/main/java/com/project/awesomegroup/service/UserService.java
@@ -1,7 +1,10 @@
 package com.project.awesomegroup.service;
 
+import com.project.awesomegroup.controller.user.UserController;
 import com.project.awesomegroup.dto.User;
 import com.project.awesomegroup.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +13,9 @@ import java.util.List;
 
 @Service
 public class UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserRepository userRepository;
 
@@ -18,5 +24,11 @@ public class UserService {
         userRepository.findAll().forEach(e -> users.add(e));
 
         return users;
+    }
+
+    public User join(User user){
+//        logger.info("${}", user);
+        userRepository.save(user);
+        return user;
     }
 }
