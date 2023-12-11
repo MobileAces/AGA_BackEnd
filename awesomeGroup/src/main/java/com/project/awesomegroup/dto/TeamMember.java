0367fd106dfaecdbc8ffd56ad000d0622664b241
habin <dhdudgns6@gmail.com>
2023-12-11 14:44:57 +0900

    user - 팀 멤버 DTO 추가 임시

---
+++ awesomeGroup/src/main/java/com/project/awesomegroup/dto/TeamMember.java
@@ -0,0 +1,29 @@
+package com.project.awesomegroup.dto;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//@Entity
+//@Data
+//@NoArgsConstructor
+//@Table(name = "teammember")
+//public class TeamMember {
+//
+//    @JoinColumn(name="", nullable = false)
+//    private String teamId;
+//
+//    @Column(nullable = false)
+//    private String userId;
+//
+//    @Column(nullable = false)
+//    private String authority;
+//
+//    @Builder
+//    public TeamMember(String teamId, String userId, String authority){
+//        this.teamId = teamId;
+//        this.userId = userId;
+//        this.authority = authority;
+//    }
+//}
